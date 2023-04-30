6db5c258620c63f3d9bb79cbc42cae6de25a862c
Dmytro <savchyndd@gmail.com>
2023-04-30 21:43:18 +0200

    final lesson 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,67 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 
-import { data } from "data/users";
-import { UsersList } from "./UsersList/UsersList";
+import { data } from 'data/users';
+import { UsersList } from './UsersList/UsersList';
+import { Button } from './Button/Button';
+import { AddUserForm } from './AddUserForm/AddUserForm';
 
 export class App extends Component {
   state = {
     users: data,
-  }
+    isFormShown: false,
+  };
 
-  deleteUser = (userId) => {
+  deleteUser = userId => {
     this.setState(prevState => ({
-      users: prevState.users.filter(user=>user.id !== userId)
-    }))
-  }
+      users: prevState.users.filter(user => user.id !== userId),
+    }));
+  };
+
+  openForm = () => {
+    this.setState({ isFormShown: true });
+  };
+
+  addUser = obj => {
+    const newUser = {
+      id: nanoid(),
+      ...obj,
+    };
+
+    this.setState(prevState => ({
+      users: [...prevState.users, newUser],
+      isFormShown: false,
+    }));
+  };
+
+  toggleJobStatus = userId => {
+    this.setState(prevState => ({
+      users: prevState.users.map(user => {
+        if (user.id !== userId) {
+          return user;
+        } else {
+          return { ...user, hasJob: !user.hasJob };
+        }
+      }),
+    }));
+  };
 
   render() {
-    const { users } = this.state;
+    const { users, isFormShown } = this.state;
 
     return (
       <>
-        <UsersList users={users} deleteUser={this.deleteUser} />
+        <UsersList
+          users={users}
+          deleteUser={this.deleteUser}
+          toggleJobStatus={this.toggleJobStatus}
+        />
+        {isFormShown ? (
+          <AddUserForm addUser={this.addUser} />
+        ) : (
+          <Button text="Add user" clickHandler={this.openForm} />
+        )}
       </>
     );
   }
-};
+}
--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,27 +1,32 @@
 import PropTypes from 'prop-types';
 
-export const UsersList = ({ users, deleteUser }) => {
-    return (
-        <ul>
-            {users.map(({ id, name, email }) => (
-                <li key={id}>
-                    <p>Name: {name}</p>
-                    <p>Email: {email}</p>
-                    <button type='button' onClick={()=>deleteUser(id)}>Delete</button>
-                </li>
-               
-            ))}
-        </ul>
-    )
+export const UsersList = ({ users, deleteUser, toggleJobStatus }) => {
+  return (
+    <ul>
+      {users.map(({ id, name, email, hasJob }) => (
+        <li key={id}>
+          <p>Name: {name}</p>
+          <p>Email: {email}</p>
+          <p>
+            Has job:{' '}
+            <span onClick={() => toggleJobStatus(id)}>{hasJob.toString()}</span>
+          </p>
+          <button type="button" onClick={() => deleteUser(id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 UsersList.propTypes = {
-    users: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            email: PropTypes.string.isRequired,
-        }).isRequired,
-    ).isRequired,
-    deleteUser: PropTypes.func.isRequired,
-}
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  deleteUser: PropTypes.func.isRequired,
+};
